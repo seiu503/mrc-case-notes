d32fef32ca83df6e42a94d5412444cca3170966a
rifkegribenes <rifkegribenes@gmail.com>
2019-02-20 21:48:39 -0800

    working on controller

---
+++ .sfdx/typings/lwc/sobjects/CaseChangeEvent.d.ts
@@ -0,0 +1,204 @@
+declare module "@salesforce/schema/CaseChangeEvent.Account" {
+  const Account:any;
+  export default Account;
+}
+declare module "@salesforce/schema/CaseChangeEvent.AccountId" {
+  const AccountId:any;
+  export default AccountId;
+}
+declare module "@salesforce/schema/CaseChangeEvent.Asset" {
+  const Asset:any;
+  export default Asset;
+}
+declare module "@salesforce/schema/CaseChangeEvent.AssetId" {
+  const AssetId:any;
+  export default AssetId;
+}
+declare module "@salesforce/schema/CaseChangeEvent.BusinessHours" {
+  const BusinessHours:any;
+  export default BusinessHours;
+}
+declare module "@salesforce/schema/CaseChangeEvent.BusinessHoursId" {
+  const BusinessHoursId:any;
+  export default BusinessHoursId;
+}
+declare module "@salesforce/schema/CaseChangeEvent.CaseNumber" {
+  const CaseNumber:string;
+  export default CaseNumber;
+}
+declare module "@salesforce/schema/CaseChangeEvent.ChangeEventHeader" {
+  const ChangeEventHeader:any;
+  export default ChangeEventHeader;
+}
+declare module "@salesforce/schema/CaseChangeEvent.ClosedDate" {
+  const ClosedDate:any;
+  export default ClosedDate;
+}
+declare module "@salesforce/schema/CaseChangeEvent.Contact" {
+  const Contact:any;
+  export default Contact;
+}
+declare module "@salesforce/schema/CaseChangeEvent.ContactId" {
+  const ContactId:any;
+  export default ContactId;
+}
+declare module "@salesforce/schema/CaseChangeEvent.CreatedBy" {
+  const CreatedBy:any;
+  export default CreatedBy;
+}
+declare module "@salesforce/schema/CaseChangeEvent.CreatedById" {
+  const CreatedById:any;
+  export default CreatedById;
+}
+declare module "@salesforce/schema/CaseChangeEvent.CreatedDate" {
+  const CreatedDate:any;
+  export default CreatedDate;
+}
+declare module "@salesforce/schema/CaseChangeEvent.Description" {
+  const Description:string;
+  export default Description;
+}
+declare module "@salesforce/schema/CaseChangeEvent.Entitlement" {
+  const Entitlement:any;
+  export default Entitlement;
+}
+declare module "@salesforce/schema/CaseChangeEvent.EntitlementId" {
+  const EntitlementId:any;
+  export default EntitlementId;
+}
+declare module "@salesforce/schema/CaseChangeEvent.HasCommentsUnreadByOwner" {
+  const HasCommentsUnreadByOwner:boolean;
+  export default HasCommentsUnreadByOwner;
+}
+declare module "@salesforce/schema/CaseChangeEvent.HasSelfServiceComments" {
+  const HasSelfServiceComments:boolean;
+  export default HasSelfServiceComments;
+}
+declare module "@salesforce/schema/CaseChangeEvent.Id" {
+  const Id:any;
+  export default Id;
+}
+declare module "@salesforce/schema/CaseChangeEvent.IsClosed" {
+  const IsClosed:boolean;
+  export default IsClosed;
+}
+declare module "@salesforce/schema/CaseChangeEvent.IsClosedOnCreate" {
+  const IsClosedOnCreate:boolean;
+  export default IsClosedOnCreate;
+}
+declare module "@salesforce/schema/CaseChangeEvent.IsEscalated" {
+  const IsEscalated:boolean;
+  export default IsEscalated;
+}
+declare module "@salesforce/schema/CaseChangeEvent.IsSelfServiceClosed" {
+  const IsSelfServiceClosed:boolean;
+  export default IsSelfServiceClosed;
+}
+declare module "@salesforce/schema/CaseChangeEvent.IsStopped" {
+  const IsStopped:boolean;
+  export default IsStopped;
+}
+declare module "@salesforce/schema/CaseChangeEvent.IsVisibleInSelfService" {
+  const IsVisibleInSelfService:boolean;
+  export default IsVisibleInSelfService;
+}
+declare module "@salesforce/schema/CaseChangeEvent.LastModifiedBy" {
+  const LastModifiedBy:any;
+  export default LastModifiedBy;
+}
+declare module "@salesforce/schema/CaseChangeEvent.LastModifiedById" {
+  const LastModifiedById:any;
+  export default LastModifiedById;
+}
+declare module "@salesforce/schema/CaseChangeEvent.LastModifiedDate" {
+  const LastModifiedDate:any;
+  export default LastModifiedDate;
+}
+declare module "@salesforce/schema/CaseChangeEvent.Origin" {
+  const Origin:string;
+  export default Origin;
+}
+declare module "@salesforce/schema/CaseChangeEvent.Owner" {
+  const Owner:any;
+  export default Owner;
+}
+declare module "@salesforce/schema/CaseChangeEvent.OwnerId" {
+  const OwnerId:any;
+  export default OwnerId;
+}
+declare module "@salesforce/schema/CaseChangeEvent.Parent" {
+  const Parent:any;
+  export default Parent;
+}
+declare module "@salesforce/schema/CaseChangeEvent.ParentId" {
+  const ParentId:any;
+  export default ParentId;
+}
+declare module "@salesforce/schema/CaseChangeEvent.Priority" {
+  const Priority:string;
+  export default Priority;
+}
+declare module "@salesforce/schema/CaseChangeEvent.Product" {
+  const Product:any;
+  export default Product;
+}
+declare module "@salesforce/schema/CaseChangeEvent.ProductId" {
+  const ProductId:any;
+  export default ProductId;
+}
+declare module "@salesforce/schema/CaseChangeEvent.Reason" {
+  const Reason:string;
+  export default Reason;
+}
+declare module "@salesforce/schema/CaseChangeEvent.ReplayId" {
+  const ReplayId:string;
+  export default ReplayId;
+}
+declare module "@salesforce/schema/CaseChangeEvent.SlaExitDate" {
+  const SlaExitDate:any;
+  export default SlaExitDate;
+}
+declare module "@salesforce/schema/CaseChangeEvent.SlaStartDate" {
+  const SlaStartDate:any;
+  export default SlaStartDate;
+}
+declare module "@salesforce/schema/CaseChangeEvent.Source" {
+  const Source:any;
+  export default Source;
+}
+declare module "@salesforce/schema/CaseChangeEvent.SourceId" {
+  const SourceId:any;
+  export default SourceId;
+}
+declare module "@salesforce/schema/CaseChangeEvent.Status" {
+  const Status:string;
+  export default Status;
+}
+declare module "@salesforce/schema/CaseChangeEvent.StopStartDate" {
+  const StopStartDate:any;
+  export default StopStartDate;
+}
+declare module "@salesforce/schema/CaseChangeEvent.Subject" {
+  const Subject:string;
+  export default Subject;
+}
+declare module "@salesforce/schema/CaseChangeEvent.SuppliedCompany" {
+  const SuppliedCompany:string;
+  export default SuppliedCompany;
+}
+declare module "@salesforce/schema/CaseChangeEvent.SuppliedEmail" {
+  const SuppliedEmail:string;
+  export default SuppliedEmail;
+}
+declare module "@salesforce/schema/CaseChangeEvent.SuppliedName" {
+  const SuppliedName:string;
+  export default SuppliedName;
+}
+declare module "@salesforce/schema/CaseChangeEvent.SuppliedPhone" {
+  const SuppliedPhone:string;
+  export default SuppliedPhone;
+}
+declare module "@salesforce/schema/CaseChangeEvent.Type" {
+  const Type:string;
+  export default Type;
+}
---
+++ .sfdx/typings/lwc/sobjects/DashboardComponent.d.ts
@@ -0,0 +1,16 @@
+declare module "@salesforce/schema/DashboardComponent.Dashboard" {
+  const Dashboard:any;
+  export default Dashboard;
+}
+declare module "@salesforce/schema/DashboardComponent.DashboardId" {
+  const DashboardId:any;
+  export default DashboardId;
+}
+declare module "@salesforce/schema/DashboardComponent.Id" {
+  const Id:any;
+  export default Id;
+}
+declare module "@salesforce/schema/DashboardComponent.Name" {
+  const Name:string;
+  export default Name;
+}
